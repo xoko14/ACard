4a9e73642c3bf28a173a107418289cd6983d91ac
xoqui <xoqui@XOKO-PC>
2021-07-18 23:40:41 +0200

    Agregar archivos de proyecto.

---
+++ ACard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ACard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
