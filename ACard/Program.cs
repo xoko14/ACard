08d461dcec0a4a1e3275cb8d950bf83460d57ed2
Xoko14 <xoquinperezb@gmail.com>
2021-07-19 10:09:30 +0200

    basic cli logic

--- ACard/Program.cs
+++ ACard/Program.cs
@@ -10,17 +10,71 @@ namespace ACard
 
         static void Main(string[] args)
         {
-            
+            switch (args[0])
+            {
+                case "add":
+                    switch (args[1])
+                    {
+                        case "game":
+                            AddGame(args[2], args[3]);
+                            break;
+                        case "profile":
+                            AddProfile(args[2], args[3]);
+                            break;
+                    }
+                    break;
+
+                case "remove":
+                    switch (args[1])
+                    {
+                        case "game":
+                            RemoveGame(args[2]);
+                            break;
+                        case "profile":
+                            RemoveProfile(args[2]);
+                            break;
+                    }
+                    break;
+
+                case "switch":
+                    SwitchProfile(args[1], args[2]);
+                    break;
+            }
         }
 
-        static void LoadData()
+        private static void LoadData()
         {
 
         }
 
-        static void SaveData()
+        private static void SaveData()
         {
             System.IO.File.WriteAllText(".\\saves.json", JsonSerializer.Serialize(games));
+        }
+
+        private static void AddGame(string name, string location)
+        {
+
+        }
+
+        private static void AddProfile(string name, string id)
+        {
+
+        }
+
+        private static void RemoveGame(string name)
+        {
+
+        }
+
+        private static void RemoveProfile(string name)
+        {
+
+        }
+
+        private static void SwitchProfile(string game, string profile)
+        {
+
         }
     }
 }
