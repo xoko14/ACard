ca363caa9462fb3526e5ab9ea4b42d4397077d1d
Xoko14 <xoquinperezb@gmail.com>
2021-09-25 15:48:11 +0200

    Clean

---
+++ ACard/Saves.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ACard
+{
+    internal class Saves
+    {
+        public List<Game> Games { get; set; }
+        public string Path { get; set; }
+
+        public Saves()
+        {
+            Games = new List<Game>();
+            Path = ".\\saves.json";
+        }
+    }
+}
